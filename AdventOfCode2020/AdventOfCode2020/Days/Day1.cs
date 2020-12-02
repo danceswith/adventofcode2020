e756ad3cd6c28084b1dfb4b283cc3d884f0714ae
Alexander Wood <awoodsey@me.com>
2020-12-02 23:24:59 +0000

    Day 1

---
+++ AdventOfCode2020/AdventOfCode2020/Days/Day1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace AdventOfCode2020.Days
+{
+    class Day1
+    {
+        public void Part1()
+        {
+            var file = File.ReadAllLines(@"D:\Projects\adventofcode2020\AdventOfCode2020\AdventOfCode2020\Days\input.txt");
+
+            foreach(var inputLine1 in file)
+            {
+                var int1 = int.Parse(inputLine1);
+
+                foreach(var inputLine2 in file)
+                {
+                    var int2 = int.Parse(inputLine2);
+
+                    if (int1 + int2 == 2020)
+                    {
+                        Console.WriteLine($"{int1 * int2}");
+                    }
+                }
+            }
+        }
+
+        public void Part2()
+        {
+            var file = File.ReadAllLines(@"D:\Projects\adventofcode2020\AdventOfCode2020\AdventOfCode2020\Days\input.txt");
+
+            foreach (var inputLine1 in file)
+            {
+                var int1 = int.Parse(inputLine1);
+
+                foreach (var inputLine2 in file)
+                {
+                    var int2 = int.Parse(inputLine2);
+
+                    foreach(var inputLine3 in file)
+                    {
+                        var int3 = int.Parse(inputLine3);
+
+                        if (int1 + int2 + int3 == 2020)
+                        {
+                            Console.WriteLine($"{int1 * int2 * int3}");
+                        }
+                    }
+                    
+                }
+            }
+        }
+    }
+}
---
+++ AdventOfCode2020/AdventOfCode2020/Program.cs
@@ -0,0 +1,28 @@
+﻿using AdventOfCode2020.Days;
+using System;
+
+namespace AdventOfCode2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            switch(input)
+            {
+                case "1":
+
+                    var day1 = new Day1();
+
+                    day1.Part1();
+                    day1.Part2();
+
+                    break;
+                default:
+                    Console.WriteLine("Nope");
+                    break;
+            }
+        }
+    }
+}
